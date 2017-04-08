c6986e66290e93d73ee42c8092211e056b495dce
liuhongliangc <758215074@qq.com>
2017-04-08 08:51:17 +0800

    Initial commit

---
+++ app/src/main/java/com/bawei/monthdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.bawei.monthdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.viewpagerindicator.TabPageIndicator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        ViewPager viewPager= (ViewPager) findViewById(R.id.main_viewpager);
+        TabPageIndicator indicator= (TabPageIndicator) findViewById(R.id.main_indicator);
+    }
+    class MyAdapter extends FragmentPagerAdapter{
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
+}
